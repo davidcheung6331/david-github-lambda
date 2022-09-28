bfae239d0bbb6f6ea68bd32471c71ca196c7593c
hkwma david <hkwma.david@gmail.com>
2022-09-28 17:41:37 +0800

    3rd msg to my wife

--- lambda_function.py
+++ lambda_function.py
@@ -5,4 +5,6 @@ def lambda_handler(event, context):
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print('1. david miss angel')
+    print('Dear my beloved wife,')
+    print('   it is another day, BB, now is 5:39pm, the wallpaper have completed 50%')
+    print('I miss you, bb')
